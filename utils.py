1e864c70251063726111d4fe0d8431861e10d6f4
BOWENmeZHENG <bowen_zheng@berkeley.edu>
2024-03-21 12:18:13 -0700

    annotate

--- utils.py
+++ utils.py
@@ -2,3 +2,13 @@ import re
 
 def split_para(para):
     return re.findall(r"[\w']+|[-.,!?;\(\)\[\]]", para)
+
+def annotate(para):
+    print(para)
+    word_list = split_para(para)
+    categories = []
+    
+    for word in word_list:
+        c = input(f"What's the category for '{word}'? ")
+        categories.append(c)
+    return word_list, categories
